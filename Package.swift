67f03c1bcd462a140d0a499f047de2b0c55b8a58
Sander <sandervantulden@gmail.com>
2023-10-12 14:57:49 +0200

    Fixed some stuff

--- Package.swift
+++ Package.swift
@@ -1,25 +1,32 @@
+// swift-tools-version:5.5
 //
 //  AttributedTextView.swift
 //  Restofire
 //
 //  Created by Edwin Vermeer on 23/10/15.
 //  Copyright © 2016 evermeer. All rights reserved.
-//
-// swift-tools-version:5.5
+
 import PackageDescription
 
-let packageName = "AttributedTextView",
+let packageName = "AttributedTextView"
 let package = Package(
     name: packageName,
     platforms: [
         .iOS(.v8),
     ],
     products: [
-        .library(name: packageName, targets: [packageName])
+        .library(
+            name: packageName,
+            targets: [packageName]
+        )
     ],
     dependencies: [
         // Stub
     ],
-    target(name: packageName, path: "AttributedTextView/Sources")
-    exclude: ["Tests"]
+    targets: [
+        .target(
+            name: packageName,
+            path: "Sources"
+        ),
+    ]
 )
